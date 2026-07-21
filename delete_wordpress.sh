61b7b8e52198104f5202b0c4d60c2dfb7cdef51f
Adam Bergh <53879585+ajbergh@users.noreply.github.com>
2026-07-21 13:43:20 +0200

    Forward legacy demo removal safely

--- delete_wordpress.sh
+++ delete_wordpress.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
-###############################
-#Name: delete_wordpress.sh
-#Author: Adam Bergh
-#Date: 07/07/2021 v1
-#Kasten by Veeam
-#Version: .1 Alpha
-################################
-
-microk8s kubectl delete ns wordpress
+#!/usr/bin/env bash
+set -Eeuo pipefail
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+printf 'NOTICE: forwarding legacy demo removal to microk10 demo remove.\n' >&2
+exec "${SCRIPT_DIR}/microk10" demo remove "$@"
