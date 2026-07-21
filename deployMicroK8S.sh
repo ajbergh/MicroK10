046d021679e4a76e33cb5d119b0159e4d4e4d87d
Adam Bergh <53879585+ajbergh@users.noreply.github.com>
2026-07-21 13:42:55 +0200

    Replace legacy MicroK8s installer with safe wrapper

--- deployMicroK8S.sh
+++ deployMicroK8S.sh
@@ -1,23 +1,5 @@
-#!/bin/bash
-###############################
-#Name: deployMicroK8s.sh
-#Author: Adam Bergh
-#Date: 07/07/2021 v1
-#Kasten by Veeam
-#Version: .1 Alpha
-################################
-
-IP_RANGE=$1
-
-deployMicroK8S() {
-#printf "${GREEN}Deploying MicroK8s\n"
-snap install microk8s --classic --channel=1.21
-sleep 1m
-microk8s enable dns registry istio helm3 host-access
-sleep 2m
-#microk8s enable metallb:${IP_RANGE}
-#sleep 30
-}
-
-deployMicroK8S
-
+#!/usr/bin/env bash
+set -Eeuo pipefail
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+printf 'NOTICE: deployMicroK8S.sh is deprecated; forwarding to microk10 install-microk8s.\n' >&2
+exec "${SCRIPT_DIR}/microk10" install-microk8s "$@"
